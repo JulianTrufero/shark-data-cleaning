aba200cb7fba131e1d4a5166c488c848ce7de14f
JulianTrufero <jgtrufero@gmail.com>
2021-08-23 01:47:23 +0200

    update

--- src/metodos-cleaning-analysis.py
+++ src/metodos-cleaning-analysis.py
@@ -1,14 +1,15 @@
-#FUNCIONES PARA LIMPIAR DATOS: HIPOTESIS I
+#FUNCIONES PARA LIMPIAR DATOS:
 
-def drop():
+ def keep_columns(x, y):
     """
-    Por cada elemento perteneciente al index de columnas, si este no pertenece a la lista de columnas
-    relevantes, lo elimina del data frame
+    Toma como argumento la lista de las columnas que quiero conservar y el nombre del data frame.
+    Recorre el indice de columnas del data frame correspondiente y elimina las
+    columnas que no quiero conservar
     """
-    relevant = ['Case Number', 'Type', 'Case Number.1', 'Case Number.2']
-    for i in sdf.columns:
+    relevant = x
+    for i in y.columns:
         if i not in relevant:
-            sdf.drop(columns=[i], inplace=True)
+            y.drop(columns=[i], inplace=True)
 
 def filtracion(x):
     """
@@ -21,18 +22,7 @@ def filtracion(x):
     if m and m[0][5:7] != '00':
         for k, v in months.items():
             if k == m[0][5:7]:
-                return v
-#FUNCIONES PARA LIMPIAR DATOS: HIPOTESIS II       
-
-def drop2():
-    """
-    Por cada elemento perteneciente al index de columnas, si este no pertenece a la lista de columnas
-    relevantes, lo elimina del data frame
-    """
-    relevant = ['Injury', 'Fatal (Y/N)', 'Species ']
-    for i in sdf2.columns:
-        if i not in relevant:
-            sdf2.drop(columns=[i], inplace=True)
+                return v     
 
 def filtracion_especies(x):
     """
